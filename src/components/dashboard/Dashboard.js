992cce9cd8470779d24dc155560bbc5be997ff66
Harish T.M <harishthirumurthi@gmail.com>
2020-10-14 05:38:32 +0530

    sorting by header values

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable react/destructuring-assignment */
@@ -17,8 +18,10 @@ class Dashboard extends React.Component {
       monitors: props.monitors,
       showHeaderIcon: false,
       visibleheader: [],
+      visibleSortHeader: [],
     };
     this.state.visibleheader = this.getVisibleHeaders();
+    this.state.visibleSortHeader = this.getVisibleSortHeaders();
   }
 
   //   componentDidMount() {
@@ -30,13 +33,66 @@ class Dashboard extends React.Component {
     return Object.keys(firstMonitor);
   };
 
+  getSortContent = (key) => {
+    const uniqueHeaderValues = this.getUniqueValuesInHeaders();
+    const uniqueItems = uniqueHeaderValues[key].map((item) => {
+      return (
+        <div
+          key={item}
+          onClick={() => {
+            this.sortMonitors(key, item);
+          }}
+          className={cx(styles['sort-item'])}
+        >
+          {item}
+        </div>
+      );
+    });
+    return <div className={cx(styles['sort-container'])}>{uniqueItems}</div>;
+  };
+
+  sortMonitors = (key, item) => {
+    const sortedMonitors = this.state.monitors;
+    sortedMonitors.sort((first, second) => {
+      debugger;
+      if (!Array.isArray(first[key])) {
+        if (first[key].includes(item) && !second[key].includes(item)) {
+          return -1;
+        }
+        if (!first[key].includes(item) && second[key].includes(item)) {
+          return 1;
+        }
+      } else if (
+        first[key].join(',') === item &&
+        !(second[key].join(',') === item)
+      ) {
+        return -1;
+      } else if (
+        !(first[key].join(',') === item) &&
+        second[key].join(',') === item
+      ) {
+        return 1;
+      }
+      return 0;
+    });
+    console.log(sortedMonitors);
+  };
+
   getHeaders = () => {
     const keys = this.getKeys();
     return keys.map((key) => {
       if (this.state.visibleheader[key].isVisible) {
         return (
-          <th key={key} className={cx(styles['table-head-row'])}>
+          <th
+            key={key}
+            onClick={() => {
+              this.toggleSortOptions(key);
+            }}
+            className={cx(styles['table-head-row'])}
+          >
             {key}
+            {this.state.visibleSortHeader[key].isVisible &&
+              this.getSortContent(key)}
           </th>
         );
       }
@@ -66,6 +122,14 @@ class Dashboard extends React.Component {
     });
   };
 
+  toggleSortOptions = (key) => {
+    const newVisibleSortHeader = this.state.visibleSortHeader;
+    newVisibleSortHeader[key].isVisible = !newVisibleSortHeader[key].isVisible;
+    this.setState(() => {
+      return { visibleSortheader: newVisibleSortHeader };
+    });
+  };
+
   getUniqueValuesInHeaders = () => {
     const uniqueHeaderValues = {};
     const keys = this.getKeys();
@@ -75,7 +139,11 @@ class Dashboard extends React.Component {
     }
     monitors.map((monitor) => {
       Object.keys(monitor).forEach((data) => {
-        if (!uniqueHeaderValues[data].includes(monitor[data])) {
+        if (Array.isArray(monitor[data])) {
+          if (!uniqueHeaderValues[data].includes(monitor[data].join(','))) {
+            uniqueHeaderValues[data].push(monitor[data].join(','));
+          }
+        } else if (!uniqueHeaderValues[data].includes(monitor[data])) {
           uniqueHeaderValues[data].push(monitor[data]);
         }
       });
@@ -92,6 +160,16 @@ class Dashboard extends React.Component {
       return key;
     });
     return visibleheader;
+  };
+
+  getVisibleSortHeaders = () => {
+    const visibleSortHeader = [];
+    const keys = this.getKeys();
+    keys.map((key) => {
+      visibleSortHeader[key] = { isVisible: false };
+      return key;
+    });
+    return visibleSortHeader;
   };
 
   getRowData = () => {
